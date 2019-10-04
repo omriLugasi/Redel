5ab2418c987c8bc0f2d3be6704f2d7cdf71b6745
omri <omlrc1991@gmail.com>
2019-10-04 13:10:32 +0300

    add new functionalities and start working on the logger module of the statistics module

--- test/statistics/index.spec.js
+++ test/statistics/index.spec.js
@@ -1,17 +1,17 @@
-const axios = require('axios')
-const ambiance = require('./../../src')
-const { BASIC_URL } = require('./../utils')
-
-const server = require('./../../server')
-server.init()
-ambiance.use(axios, { statistics: true })
-axios.get(`${BASIC_URL}/time-out/900?io=omri lugasi the king`)
-  .then(() => {
-    server.close()
-  })
-  .catch(() => {
-    server.close()
-  })
+// const axios = require('axios')
+// const ambiance = require('./../../src')
+// const { BASIC_URL } = require('./../utils')
+//
+// const server = require('./../../server')
+// server.init()
+// ambiance.use(axios, { statistics: true })
+// axios.get(`${BASIC_URL}/time-out/900?io=omri lugasi the king`)
+//   .then(() => {
+//     server.close()
+//   })
+//   .catch(() => {
+//     server.close()
+//   })
 
 // axios.post(`${BASIC_URL}/time-out/900`, { param1: true })
 //   .then(() => {
