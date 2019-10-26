2b8f8a26e2922b6c715fac238b7e088b53cbe71c
omri <omlrc1991@gmail.com>
2019-10-26 15:17:33 +0300

    - readme fixes - add test to check if getCustomCancelHeader got any injected param before returing object

--- test/cancel/index.spec.js
+++ test/cancel/index.spec.js
@@ -66,7 +66,6 @@ describe('Cancel plugin', () => {
     })
 
     context('throw an error if developer try to use get header function without inject any key', () => {
-
       it('should validate that get headers without key throw exception', () => {
         assert.throw(() => Redel.getCancelGroupHeader(), `"getCancelGroupHeader" should invoke 
       with cancel group key, please verify that you didn't 
